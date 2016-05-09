9a379a89e1dad857b185363d21af7b1af824e606
Robert Black <rbla4655@gmail.com>
2016-05-09 15:05:50 -0400

    jaws db

--- config/connection.js
+++ config/connection.js
@@ -5,4 +5,10 @@ var con = mysql.createConnection({
    password: 'Coolbeans777',
    database: 'burger_db'
 });
-module.exports = con;
+var jaws = mysql.createConnection({
+	host: "l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	user: "myuw4w0u61srtwks",
+	password: "ghenzo0wriau3tzv",
+	database: "t6gtu624abb8fthv"
+});
+module.exports = jaws;
