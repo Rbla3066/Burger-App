5bb1ee957afd809d2e579e01470b2856dca51010
Robert Black <rbla4655@gmail.com>
2016-05-09 15:16:40 -0400

    complete

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,4 @@
 var mysql = require("mysql");
-var con = mysql.createConnection({
-   host: 'localhost',
-   user: 'public',
-   password: 'Coolbeans777',
-   database: 'burger_db'
-});
 var jaws = mysql.createConnection({
 	host: "l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 	user: "myuw4w0u61srtwks",
